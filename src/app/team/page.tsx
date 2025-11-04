b779486f1b85bfdc825ddce3e8c7156293978d0f
Akshith Rajesh <akshitrajesh2020@gmail.com>
2025-11-04 17:37:46 +0000

    fix: image size

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -50,7 +50,7 @@ export default function TeamPage() {
           </div>
 
           <div className="mt-16 mb-24">
-            <div className="max-w-5xl mx-auto">
+            <div className="max-w-4xl mx-auto">
               <div className="relative aspect-[16/9] rounded-2xl overflow-hidden shadow-2xl shadow-primary/20 border border-primary/30">
                 <Image
                   src="/team/team.png"
