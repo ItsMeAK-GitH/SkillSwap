bf4970cca5ffa6edd0f6b7159b455c67a9567dfc
Akshith Rajesh <akshitrajesh2020@gmail.com>
2025-11-05 16:37:08 +0000

    fix:server error

--- .next/static/chunks/src_app_chat_[chatId]_page_tsx_90233e98._.js
+++ .next/static/chunks/src_app_chat_[chatId]_page_tsx_90233e98._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_c6f81493._.js",
-  "static/chunks/node_modules_f37a1028._.js"
+  "static/chunks/src_f235ba36._.js",
+  "static/chunks/node_modules_3a527724._.js"
 ],
     source: "dynamic"
 });
